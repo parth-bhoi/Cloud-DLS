1f639278fe11ab94291ed61502c8dfff32295d4e
Bhargav Patel <56644569+bhargav-patel15@users.noreply.github.com>
2021-08-06 17:54:34 +0530

    Add files via upload

---
+++ FCFS/VmsCreator.java
@@ -0,0 +1,43 @@
+package org.cloudbus.cloudsim.examples;
+
+import java.util.ArrayList;
+
+import org.cloudbus.cloudsim.CloudletSchedulerTimeShared;
+import org.cloudbus.cloudsim.Vm;;
+
+
+/**
+ * VmsCreator Creates VM Lists as per the User Requirements.
+ * @author Linda J
+ *
+ */
+public class VmsCreator {
+	
+	//vmlist creator function
+	public ArrayList<Vm> createRequiredVms(int reqVms, int brokerId){
+		
+		ArrayList<Vm> vmlist = new ArrayList<Vm>();
+		
+    	//VM description
+    	int vmid = 0;
+    	int mips = 1000;
+    	long size = 10000; //image size (MB)
+    	int ram = 512; //vm memory (MB)
+    	long bw = 1000;
+    	int pesNumber = 1; //number of cpus
+    	String vmm = "Xen"; //VMM name
+
+    	
+    	
+    	for(vmid=0;vmid<reqVms;vmid++){
+    		//add the VMs to the vmList
+    		vmlist.add(new Vm(vmid, brokerId, mips, pesNumber, ram, bw, 
+    				size, vmm, new CloudletSchedulerTimeShared()));
+    	}
+
+    	System.out.println("VmsCreator function Executed... SUCCESS:)");
+		return vmlist;
+		
+	}
+
+}
